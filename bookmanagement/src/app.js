2710656d281bb744a40e2d04ce20d398013e060d
JNevile <j.neville657@mybvc.ca>
2024-12-05 14:54:39 -0700

    updates to app.js

--- bookmanagement/src/app.js
+++ bookmanagement/src/app.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const { fileAuth, generateJWT } = require('../auth  ');
+const booksRouter = require('./routes/books');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const app = express();
 
-export default App;
+//json request bodies through middleware
+app.use(express.json());
+
+//CORS middleware
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+//Login for admin user
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    //User authentication
+    const user = fileAuth(username, password);
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    //Generate a JWT for the authenticated user
+    const token = generateJWT(username);
+
+    res.json({ message: 'Welcome! You are now logged in', token });
+});
+
+//Handle book-related requests
+app.use('/books', booksRouter);
+
+module.exports = app;
