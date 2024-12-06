f439fa34d7349ac4180538623040a36248e01095
Aaron Amoso <amosoaaron@gmail.com>
2024-12-05 17:36:41 -0700

    edited server so port is clickable to open a window/web browser

---
+++ bookmanagement/src/App.jsx
@@ -0,0 +1,15 @@
+// THIS IS WHERE THE APP IS RENDERED. IN OTHER WORDS, WE PLACE ALL THE COMPONENTS ON HERE TO SHOW ON THE WEB ONCE THE APP IS LOADED
+
+import React from 'react'
+import Login from './admin/login'
+
+function App() {
+  return (
+    <div>
+        {/* Login component */}
+      <Login /> 
+    </div>
+  )
+}
+
+export default App
--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.use('/books', booksRouter);
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
