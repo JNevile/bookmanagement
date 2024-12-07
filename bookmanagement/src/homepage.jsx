879be8109b6b140580f766499fc5bf57acafbaaa
JNevile <j.neville657@mybvc.ca>
2024-12-06 23:17:03 -0700

    Update code for homepage.jsx

--- bookmanagement/src/homepage.jsx
+++ bookmanagement/src/homepage.jsx
@@ -1,1 +1,17 @@
 
+//import React, { useState } from 'react';
+//const Homepage = () => {
+  //const [isAdmin, setIsAdmin] = useState(false);
+    //const handleLogin = () => {
+       
+      //setIsAdmin(true);
+       //alert('Logged in as Admin');
+    //};
+    //return (
+      //  <div>
+        //    <h1>Welcome to the Book Management System</h1>
+        //    {!isAdmin && <button onClick={handleLogin}>Login as Admin</button>}
+        //</div>
+    //);
+//};
+//export default Homepage;
